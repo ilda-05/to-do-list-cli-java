db4f356fa5d9ae753313795efbb2f99e980fb2f5
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2026-01-08 17:42:47 +0100

    starting project

---
+++ todolist/src/main/java/com/ilda/Main.java
@@ -0,0 +1,66 @@
+package com.ilda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        List<String> task_list = new ArrayList<String>();;
+
+        System.out.println("0 Exit");
+        System.out.println("1 View all the tasks");
+        System.out.println("2 Add Task");
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean on_going = true;
+
+
+        while(on_going){
+
+        System.out.print("\nUser >> ");
+        String user_opt = scanner.nextLine();
+
+        switch (user_opt) {
+            case "0":
+                
+                on_going=false;
+                break;
+
+            case "1":
+
+                if(task_list.isEmpty())
+                    System.out.println("There's no task");
+                else{
+                    for(String task : task_list){
+                        System.out.println(task);
+                    }
+                }
+
+                break;
+
+            case "2":
+
+
+                System.out.print("Task's Description : ");
+
+                task_list.add(scanner.nextLine());
+                
+                System.out.println("Task Created");
+
+
+
+                break;
+        
+            default:
+                
+                System.out.println("Choose an valid option");
+        }
+
+        }
+
+    }
+}
