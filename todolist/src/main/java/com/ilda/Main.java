290157221897e0fdfb3b9c5299eada79d0834e87
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2026-01-08 23:41:28 +0100

    delete task feature

--- todolist/src/main/java/com/ilda/Main.java
+++ todolist/src/main/java/com/ilda/Main.java
@@ -2,7 +2,6 @@ package com.ilda;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
@@ -10,16 +9,19 @@ public class Main {
     
         List<String> task_list = new ArrayList<String>();;
 
-        System.out.println("0 Exit");
-        System.out.println("1 View all the tasks");
-        System.out.println("2 Add Task");
-
         Scanner scanner = new Scanner(System.in);
 
         boolean on_going = true;
 
 
         while(on_going){
+
+        // CLI Menu'
+
+        System.out.println("0 Exit");
+        System.out.println("1 View All The Tasks");
+        System.out.println("2 Add Task");
+        System.out.println("3 Delete Task");
 
         System.out.print("\nUser >> ");
         String user_opt = scanner.nextLine();
@@ -33,7 +35,7 @@ public class Main {
             case "1":
 
                 if(task_list.isEmpty())
-                    System.out.println("There's no task");
+                    System.out.println("There's no task\n");
                 else{
                     int i = 1;
                     for(String task : task_list){
@@ -50,12 +52,37 @@ public class Main {
 
                 task_list.add(scanner.nextLine());
                 
-                System.out.println("Task Created");
+                System.out.println("Task Created\n");
 
 
 
                 break;
-        
+
+            case "3":
+
+                if(task_list.isEmpty()){
+                    System.out.println("There's no task to delete\n");
+                    break;
+                }
+                else{
+                    int i = 1;
+                    for(String task : task_list){
+                        System.out.println(i++ + " " + task);
+                    }
+                }
+
+                System.out.print("What task do you want to delete? : ");
+                
+                try {
+                    int task_to_remove=Integer.parseInt(scanner.nextLine())-1;
+                    task_list.remove(task_to_remove);
+                    System.out.println("Task Deleted\n");
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Invalid Input\n");
+                    break;
+                }
+
             default:
                 
                 System.out.println("Choose an valid option");
