385c2bc82867297a2a43edc7791159b627dcf499
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2026-01-09 12:57:53 +0100

    getters setters task

--- todolist/src/main/java/com/ilda/models/Task.java
+++ todolist/src/main/java/com/ilda/models/Task.java
@@ -14,4 +14,36 @@ public class Task {
 
     }
 
+    public String getTaskName(){
+
+        return this.name;
+
+    }
+
+    public String getTaskDescription(){
+
+        return this.description;
+
+    }
+
+    public boolean getTaskChecked(){
+
+        return this.checked;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    
+
 }
